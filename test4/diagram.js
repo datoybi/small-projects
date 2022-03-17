412411e9b23fff5fd6b7de9d643aa79ff63ce5f9
Dasom Yun <41810621+datoybi@users.noreply.github.com>
2022-03-17 17:46:49 +0900

    선긋기 클릭시 두개의 박스 선택 구현

--- test4/diagram.js
+++ test4/diagram.js
@@ -1,6 +1,9 @@
 
 // 함수 표현식을 사용하고 싶으면 클로저를 사용해야한다. 호이스팅이 안되기 때문!!! 나중에 클로저 공부해서 적용시키기
 // https://www.w3schools.com/howto/howto_js_draggable.asp
+let line = false;
+let lines = [];
+let boxNum = 3;
 
 function dragElement(e) {
     let elmnt = e.target;
@@ -27,27 +30,77 @@ function dragElement(e) {
     divY = (elmnt.offsetTop - pos2);
     elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
     elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-    console.log('divX : ' + divX, + " , divY : " + divY);
+    // console.log('divX : ' + divX, + " , divY : " + divY);
   }
-
   function closeDragElement() {
     document.onmouseup = null;
     document.onmousemove = null;
   }
 }
 
+function lineElement(){
+
+  // document.querySelectorAll('.mydiv').classList.remove('line_clicked');
+  delBoxColor();
+  line = true;
+  lines = [];  
+}
+
+function makeLine() {
+
+}
+
 function createBox() {
     let div = document.createElement("div");
     div.classList.add("mydiv");
+    div.id = `mydiv${boxNum}`;
     div.setAttribute("onmousedown", "dragElement(event)");
     document.body.appendChild(div);
+    boxNum ++;
 }
+
+function delBoxColor() {
+  let mydiv = document.querySelectorAll('.mydiv')
+  for(div of mydiv) {
+    div.classList.remove('line_clicked');
+  }
+}
+// function createLine() {
+//   console.log('line : ' + line)
+//   line = true; 
+// }
+
 
 window.onload = function() {
   window.onclick = (event) => {
     let x = event.clientX;
     let y = event.clientY;
-    
-    console.log(`마우스 클릭 좌표 (${x}, ${y})`);
+    // console.log(`마우스 클릭 좌표 (${x}, ${y})`);
+    // console.log(event.target)
+   
+    let clicked = event.target.classList[0];
+    if(line === true) {
+      if(clicked === 'mydiv' || event.target.id === 'createLine'){
+        if (event.target.id !== 'createLine'){
+          lines.push(event.target.id);
+          document.querySelector(`#${event.target.id}`).classList.add('line_clicked');
+        } 
+        if (lines.length === 2){
+          makeLine();
+        } else if (lines.length > 2) {
+          line = false
+          lines = []
+          delBoxColor()
+
+        }
+      } else { // 다른곳 클릭했을 경우 
+        line = false
+        lines = []
+        delBoxColor()
+      }
+    }
+    console.log(lines)  
+
+ 
   }
 }
